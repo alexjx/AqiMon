4b0a0b1b9c28a140922dd8ec597b6d704c5124d3
AlexXJ <alexjx@gmail.com>
2013-05-15 14:43:12 +0800

    Trivial modifications. Define pins

--- AirQ.cpp
+++ AirQ.cpp
@@ -11,7 +11,7 @@
  * 	DHT22	DATA 	-> A1
  * 	DSM501	PM10 	-> A3
  * 			PM25 	-> A2
- * 	DS1307	SDL		-> SDL
+ * 	DS1307	SDA		-> SDL
  * 			SCL		-> SCL
  * 	LCD1602	RS		-> 10
  * 			RW		-> 9
@@ -29,6 +29,24 @@
  *  		TX		-> 1
  */
 
+#define DHT22_PIN	A1
+#define DSM501_PM10	A3
+#define DSM501_PM25	A2
+#define DS1307_SDA	SDA
+#define DS1307_SCL	SCL
+#define LCD_RS		10
+#define LCD_RW		9
+#define LCD_E		A0
+#define LCD_D4		5
+#define LCD_D5		6
+#define LCD_D6		7
+#define LCD_D7		8
+#define SD_CS		4
+#define SD_MISO		MISO
+#define SD_MOSI		MOSI
+#define SD_SCK		SCK
+
+
 // data
 struct AirQMsg {
 	float temperature;
@@ -43,26 +61,27 @@ struct AirQMsg {
 #define SSPEED 	115200
 
 ulong_t lastUpd = 0u;
-ulong_t lcdIntv = 1050u;	// 1s
+ulong_t lcdIntv = 1000u;	// 1s
 
 ulong_t lastLog = 0u;
 ulong_t logIntv = DSM501_MIN_WIN_SPAN;
 
-char buf[64]; // display buffer
+char buf_t[32]; // time buffer
+char buf_d[32]; // display buffer
 
 
 /***********************************************
  * Components
  ***********************************************/
 DS1307 ds1307;
-DHT22 dht(A1);
+DHT22 dht(DHT22_PIN);
 DSM501* dsm501 = NULL;
-LiquidCrystal lcd(10, 9, A0, 5, 6, 7, 8);
+LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 /***********************************************
- * Global Control variable
+ * Report function
  ***********************************************/
-int Report(char *buf, bool detail = false) {
+int genReports(char *buf, bool detail = false) {
 	char buf1[16];
 	int n = 0;
 
@@ -110,7 +129,9 @@ void setup() {
 }
 
 
-
+/***********************************************
+ * Main Loop
+ ***********************************************/
 void loop() {
 	// call dsm501 to handle updates.
 	dsm501->update();
@@ -121,16 +142,16 @@ void loop() {
 			int val = Serial.read();
 			switch(val) {
 			case 'r':
-				ds1307.makeStr(buf);
-				Serial.print(buf);
+				ds1307.makeStr(buf_t);
+				Serial.print(buf_t);
 				Serial.print(" ");
-				Report(buf, true);
-				Serial.println(buf);
+				genReports(buf_d, true);
+				Serial.println(buf_d);
 				break;
 
 			case 't':
-				ds1307.makeStr(buf);
-				Serial.println(buf);
+				ds1307.makeStr(buf_t);
+				Serial.println(buf_t);
 				break;
 
 			case 'T':
@@ -166,33 +187,43 @@ void loop() {
 				break;
 			}
 		}
+	} // Serial
 
-		if (millis() - lastUpd > lcdIntv) {
-			lcd.setCursor(0, 0);
-			ds1307.makeStr(buf);
-			lcd.print(buf);
+	/*
+	 * Update LCD every seconds
+	 */
+	if (millis() - lastUpd > lcdIntv) {
+		lcd.setCursor(0, 0);
+		ds1307.makeStr(buf_t);
+		lcd.print(buf_t);
 
-			lcd.setCursor(0, 1);
-			Report(buf);
-			lcd.print(buf);
+		lcd.setCursor(0, 1);
+		genReports(buf_d);
+		lcd.print(buf_d);
 
-			lastUpd = millis();
+		lastUpd = millis();
+	}
+
+	/*
+	 * Log data to SD card if possible.
+	 */
+	if (millis() - lastLog > logIntv) {
+		File dataFile = SD.open("aqi_log.txt", FILE_WRITE);
+
+		if (dataFile) {
+			// since LCD is updating this every sec, we skip this
+//			ds1307.makeStr(buf_t);
+			dataFile.print(buf_t);
+
+			dataFile.print(" "); // Delimiter
+
+			// log more detail info
+			genReports(buf_d, true);
+			dataFile.println(buf_d);
+
+			dataFile.close();
 		}
 
-		if (millis() - lastLog > logIntv) {
-			File dataFile = SD.open("aqi_log.txt", FILE_WRITE);
-			if (dataFile) {
-				ds1307.makeStr(buf);
-				dataFile.print(buf);
-
-				dataFile.print(" "); // delim
-
-				Report(buf, true);
-				dataFile.println(buf);
-
-				dataFile.close();
-			}
-			lastLog = millis();
-		}
+		lastLog = millis();
 	}
 }
